0612f84aab0095b3b2731fc3e316a930b184b316
ismailtaher <ismail.taher@ymail.com>
2023-09-30 19:12:26 +0500

    4th Commit

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
 // Event Listeners JavaScript
+
+const view = document.querySelector("#view2");
+const div = view.querySelector("div");
+const h2 = div.querySelector("h2");
+
+// Syntax: addEventListener(event, function, useCapture)
+
+const doSomething = () => {
+  alert("Doing Something");
+};
+
+h2.addEventListener("click", doSomething, false);
+h2.removeEventListener("click", doSomething, false);
